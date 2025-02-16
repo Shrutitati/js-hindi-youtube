18c5f18898a7b02d8149b9b1d2690223c08548c9
Shrutitati <92931261+Shrutitati@users.noreply.github.com>
2025-02-16 07:31:27 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 112222
+let accountEmail = 'gddgdg@gmail.com'
+var accountPassowrd = 'dhg$333'
+accountity= 'Gulbarga'
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassowrd)
+console.log(accountity)
+
+console.table([accountId, accountEmail, accountPassowrd, accountity])
